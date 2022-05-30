28216905e4f2bbfe2a44f83f5d48903507c1eeca
syedafazilathjahaa <104976934+syedafazilathjahaa@users.noreply.github.com>
2022-05-30 15:35:40 +0530

    Update build.sh

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,8 +1,8 @@
-RUN cd AFL
-RUN make
-RUN cd llvm_mode
-RUN sudo apt-get install -y clang llvm-dev
-RUN set $LLVM_CONFIG=llvm-config-3.5
-RUN sudo make
-RUN cd ..
-RUN sudo make install
+cd AFL
+make
+cd llvm_mode
+sudo apt-get install -y clang llvm-dev
+set $LLVM_CONFIG=llvm-config-3.5
+sudo make
+cd ..
+sudo make install
