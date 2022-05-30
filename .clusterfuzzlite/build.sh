d386b572515b55204163379e7065787666fa4f3c
syedafazilathjahaa <104976934+syedafazilathjahaa@users.noreply.github.com>
2022-05-30 16:23:26 +0530

    Update build.sh

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,7 +1,7 @@
 make
 cd llvm_mode
-apt-get install -y clang llvm-dev
-set $LLVM_CONFIG=llvm-config-10
+RUN apt-get install -y clang llvm-dev
+set $LLVM_CONFIG=/usr/bin/llvm-config-10
 make
 cd ..
 sudo make install
