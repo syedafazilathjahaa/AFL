7923c5271e3535e730f5e10a295fb7b9123c0fb9
syedafazilathjahaa <104976934+syedafazilathjahaa@users.noreply.github.com>
2022-05-30 15:06:22 +0530

    Update build.sh

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,1 +1,7 @@
-AFL_HARDEN=1 ./afl-gcc -fsanitize=address ReadFile/imgRead_Fuzzer.c -o ReadFile/imgRead_Fuzzer
+RUN make
+RUN cd llvm_mode
+RUN sudo apt-get install -y clang llvm-dev
+          set $LLVM_CONFIG=llvm-config-3.5
+          sudo make
+          cd ..
+          sudo make install
